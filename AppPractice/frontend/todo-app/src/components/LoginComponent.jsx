9c18a74e2cbe246b3a1ef764700b1ed65c3e9577
Sonal <kamblesonal893@gmail.com>
2022-03-31 13:21:17 +0530

    component2===> LoginComponent.jsx ---> loginClicked

--- AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
+++ AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
@@ -5,21 +5,39 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: 'in28minutes',
-            password: ''
+            password: '',
+            showSuccessMessage: false,
+            hasLoginFailed: false
         }
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
 
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value})
     }
 
+    loginClicked() {
+        if(this.state.username==="in28minutes" && this.state.password==="dummy")
+        {
+            this.setState({showSuccessMessage: true})
+            this.setState({hasLoginFailed: false})
+        }
+        else 
+        {
+            this.setState({hasLoginFailed: true})
+            this.setState({showSuccessMessage: false})
+        }
+    }
+
     render() {
         return (
             <div className="container">
+                {this.state.showSuccessMessage && <div>Login Successful</div>}
+                {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
                 Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                <button className='btn btn'>Login</button>
+                <button className='btn btn' onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
