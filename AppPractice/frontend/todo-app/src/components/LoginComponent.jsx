fa917a74dea1c57eb10683a6a4044b7d72f4c248
Sonal <kamblesonal893@gmail.com>
2022-03-31 13:14:32 +0530

    component2===> LoginComponent.jsx ---> handleChange

--- AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
+++ AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
@@ -2,18 +2,23 @@ import React, {Component} from 'react'
 
 class LoginComponent extends Component {
     constructor(props) {
-        super(props) 
+        super(props)
         this.state = {
             username: 'in28minutes',
             password: ''
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value})
     }
 
     render() {
         return (
             <div className="container">
-                Username: <input type="text" name="username" value={this.state.username}></input>
-                Password: <input type="password" name="password" value={this.state.password}></input>
+                Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button className='btn btn'>Login</button>
             </div>
         )
