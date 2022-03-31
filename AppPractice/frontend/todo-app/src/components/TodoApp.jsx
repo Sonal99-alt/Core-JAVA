d58c7305747b314c5edba34f7119286a71963da2
Sonal <kamblesonal893@gmail.com>
2022-03-31 13:30:00 +0530

    component4===> WithParams.jsx

--- AppPractice/frontend/todo-app/src/components/TodoApp.jsx
+++ AppPractice/frontend/todo-app/src/components/TodoApp.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import LoginComponent from './LoginComponent'
 import withNavigation from './WithNavigation'
 import withParams from './WithParams'
+import WelcomeComponent from './WelcomeComponent'
+
 class TodoApp extends Component {
     render() {
         const WelcomeComponentWithParams = withParams(WelcomeComponent)
---
+++ AppPractice/frontend/todo-app/src/components/WelcomeComponent.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class WelcomeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome!</h1>
+                <div>
+                    Welcome
+                </div>
+            </div>
+        )
+    }
+}
+export default WelcomeComponent
