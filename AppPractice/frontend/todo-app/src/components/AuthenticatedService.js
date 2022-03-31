2e5fe98f09b4a4c46e1f4bad705861bc6d1bc1a9
Sonal <kamblesonal893@gmail.com>
2022-03-31 13:57:57 +0530

    component7===> AuthenticatedService.js

---
+++ AppPractice/frontend/todo-app/src/components/AuthenticatedService.js
@@ -0,0 +1,18 @@
+class AuthenticatedService  {
+    registerSuccessfulLogin(username, password) {
+        sessionStorage.setItem('authenticatedUser', username)
+    }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser')
+    }
+
+    isUserLoggedIn() {
+        let user = sessionStorage.getItem('authenticatedUser')
+        if(user == null)
+        return false
+        return true;
+    }
+}
+
+export default AuthenticatedService
