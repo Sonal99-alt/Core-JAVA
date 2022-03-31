55d479e4290626fc18c80c00700c965a59100cc2
Sonal <kamblesonal893@gmail.com>
2022-03-31 13:04:21 +0530

    component2===> LoginComponent.jsx ---> constructor

--- AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
+++ AppPractice/frontend/todo-app/src/components/LoginComponent.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 
 class LoginComponent extends Component {
-    
+    constructor(props) {
+        super(props) 
+        this.state = {
+            username: 'in28minutes',
+            password: ''
+        }
+    }
 
     render() {
         return (
             <div className="container">
-                Username: <input type="text" name="username"></input>
-                Password: <input type="password" name="password"></input>
+                Username: <input type="text" name="username" value={this.state.username}></input>
+                Password: <input type="password" name="password" value={this.state.password}></input>
                 <button className='btn btn'>Login</button>
             </div>
         )
